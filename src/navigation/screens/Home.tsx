bf603ab515d9f3e5da4e814ac2f50ee939c3d9f6
orchlonn <ch.orchlon@gmail.com>
2025-01-09 14:35:25 -0800

    wip: filter notf UI

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,42 +1,79 @@
 import React, { useState, useEffect } from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 import { Button, Text } from "@react-navigation/elements";
 import { Input } from "@rneui/base";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TextInput } from "react-native-paper";
 import { sharedStyles } from "../../utils/styles";
+import filter from "../../assets/filter.png";
+import { Image } from "@rneui/themed";
 
 export function Home() {
-  const [guestName, setGuestName] = useState("");
+  const [searchBox, setSearchBox] = useState("");
   var shared: any = sharedStyles();
   return (
-    <SafeAreaView edges={["top", "left", "right"]} style={styles.container}>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center", // Vertically center the items
+          justifyContent: "space-between",
+          gap: 10,
+        }}
+      >
         <Input
-          value={guestName}
+          value={searchBox}
           placeholder="Search guests"
-          onChangeText={(text) => setGuestName(text)}
+          onChangeText={(text) => setSearchBox(text)}
+          containerStyle={{
+            backgroundColor: shared.white.color,
+            borderRadius: 15,
+            width: wp(70),
+            height: 45,
+          }}
           inputContainerStyle={{
             borderBottomWidth: 0,
-            borderRadius: 10,
-            paddingHorizontal: 20,
-            backgroundColor: shared.white.color,
           }}
           leftIcon={
             <TextInput.Icon
               icon="magnify"
               style={{
                 pointerEvents: "none",
-                marginRight: 0,
+                marginLeft: 15,
               }}
               color={"#A6A3B8"}
             />
           }
-          style={{ paddingLeft: 15 }}
+          style={{
+            paddingLeft: 20,
+          }}
         />
-        <View></View>
-        <Text>Home Screen</Text>
+        <View style={{ backgroundColor: "red" }}>
+          <Image
+            source={filter}
+            style={{
+              width: 24,
+              height: 20,
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+        <View style={{ backgroundColor: "black" }}>
+          <Image
+            source={filter}
+            style={{
+              width: 24,
+              height: 20,
+              resizeMode: "contain",
+            }}
+          />
+        </View>
       </View>
+      <Text>Home Screen</Text>
     </SafeAreaView>
   );
 }
@@ -44,7 +81,8 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 10,
+    width: wp(100),
     backgroundColor: "#F9F9FF",
+    paddingHorizontal: wp(3),
   },
 });
