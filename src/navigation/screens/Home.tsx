52eeb91ad65439133ff9a7b99ba5331c9aa521ae
orchlonn <ch.orchlon@gmail.com>
2025-01-12 19:02:14 -0800

    Category list on Home Screen UI

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -3,29 +3,33 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import { Text } from "@rneui/themed";
-import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../components/Home/Header";
 import { sharedTextStyles } from "../../utils/textStyles";
 import ViewAll from "../../components/Home/ViewAll";
+import { categoryData } from "../../utils/categoryData";
+import CategoryList from "../../components/Home/CategoryList";
+import { makeStyles } from "@rneui/themed";
 
 export function Home() {
   var textStyles: any = sharedTextStyles();
+  const style = styles();
+  const data = categoryData;
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={style.container}>
       <Header />
       <ViewAll />
+      <CategoryList />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = makeStyles((theme) => ({
   container: {
     flex: 1,
     width: wp(100),
     backgroundColor: "#F9F9FF",
     paddingHorizontal: wp(3),
   },
-});
+}));
---
+++ src/utils/categoryData.ts
@@ -0,0 +1,39 @@
+import cleaning from "../assets/categories/cleaning.png";
+import electrician from "../assets/categories/electrician.png";
+import fittings from "../assets/categories/fittings.png";
+import painting from "../assets/categories/painting.png";
+import roofing from "../assets/categories/roofing.png";
+import plumbing from "../assets/categories/plumbing.png";
+
+export const categoryData = [
+  {
+    bgColor: "#f2a775",
+    Category: "Roofing",
+    imageUrl: roofing,
+  },
+  {
+    bgColor: "#52ab84",
+    Category: "Electrician",
+    imageUrl: electrician,
+  },
+  {
+    bgColor: "#f5bf41",
+    Category: "Cleaning",
+    imageUrl: cleaning,
+  },
+  {
+    bgColor: "#db5d85",
+    Category: "Fittings",
+    imageUrl: fittings,
+  },
+  {
+    bgColor: "#5ea3f8",
+    Category: "Plumbing",
+    imageUrl: plumbing,
+  },
+  {
+    bgColor: "#d0398c",
+    Category: "Painting",
+    imageUrl: painting,
+  },
+];
