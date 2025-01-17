bf81117ba93766a5d6485fa4368da71b9a080c37
orchlonn <ch.orchlon@gmail.com>
2025-01-16 16:21:46 -0800

    added handy Card

--- src/components/HandyCard.tsx
+++ src/components/HandyCard.tsx
@@ -7,49 +7,49 @@ import { sharedStyles } from "../utils/styles";
 import { Image } from "@rneui/themed";
 import heartIcon from "../assets/heart.png";
 import starIcon from "../assets/star.png";
+import mockImage from "../assets/mock_image.jpg";
 import { makeStyles, Text } from "@rneui/themed";
+import { sharedTextStyles } from "../utils/textStyles";
 
 const HandyCard = () => {
   var shared: any = sharedStyles();
   const style = styles();
+  const textStyles: any = sharedTextStyles();
 
   const textInfo = () => {
     return (
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginHorizontal: 10,
-          marginTop: 5,
-        }}
-      >
+      <View style={style.textInfoContainer}>
         <TouchableOpacity>
-          <Image
-            source={heartIcon}
-            style={{
-              width: wp(3),
-              height: wp(3),
-              resizeMode: "contain",
-            }}
-          />
+          <Image source={heartIcon} style={style.imgStyle} />
         </TouchableOpacity>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text>4.9</Text>
-          <Image
-            source={starIcon}
-            style={{
-              width: wp(3),
-              height: wp(3),
-              resizeMode: "contain",
-            }}
-          />
+          <Image source={starIcon} style={style.imgStyle} />
         </View>
       </View>
     );
   };
 
-  return <View style={[style.card, style.shadowProp]}>{textInfo()}</View>;
+  const handyImage = () => {
+    return (
+      <View style={style.imgContainer}>
+        <View style={style.handyImage}>
+          <Image source={mockImage} style={style.cardImage} />
+        </View>
+        <View style={style.nameAndRate}>
+          <Text style={textStyles.whiteMediumRegular}>John Smith</Text>
+          <Text style={textStyles.whiteSmallRegular}>$20.00/hr</Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={[style.card, style.shadowProp]}>
+      {textInfo()}
+      {handyImage()}
+    </View>
+  );
 };
 
 const styles = makeStyles((theme) => ({
@@ -58,7 +58,6 @@ const styles = makeStyles((theme) => ({
     borderRadius: 8,
     marginVertical: 10,
     width: wp(25),
-    height: hp(10),
   },
   shadowProp: {
     shadowColor: "#171717",
@@ -66,6 +65,44 @@ const styles = makeStyles((theme) => ({
     marginLeft: 4,
     shadowOpacity: 0.2,
     shadowRadius: 3,
+  },
+  textInfoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 10,
+    marginTop: 5,
+  },
+  imgStyle: {
+    width: wp(3),
+    height: wp(3),
+    resizeMode: "contain",
+  },
+  cardImage: {
+    width: wp(19),
+    height: wp(19),
+    borderRadius: 100,
+  },
+  nameAndRate: {
+    marginTop: wp(12),
+    alignItems: "center",
+    gap: 3,
+  },
+  handyImage: {
+    width: wp(19),
+    height: wp(19),
+    backgroundColor: "grey",
+    borderRadius: 100,
+    position: "absolute",
+    top: -wp(9.5),
+    left: wp(3),
+  },
+  imgContainer: {
+    backgroundColor: "orange",
+    alignItems: "center",
+    height: hp(11),
+    borderRadius: 8,
+    marginTop: wp(10),
   },
 }));
 
