c552ba11b6fbf58393e485b25d652ef92f2d5c0d
orchlonn <ch.orchlon@gmail.com>
2025-04-10 16:41:00 -0700

    file structure update

---
+++ src/components/BookJob/PageMark.tsx
@@ -0,0 +1,84 @@
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import React from "react";
+import { Icon, makeStyles, Text } from "@rneui/themed";
+import { TextStyle, View, ViewStyle } from "react-native";
+import { sharedStyles } from "../../utils/styles";
+
+export function PageMark() {
+  const style = styles();
+  const shared: any = sharedStyles();
+
+  const outerCircle = (isTrue: boolean): ViewStyle => ({
+    height: wp(8),
+    width: wp(8),
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: isTrue ? "#f7c9aa" : shared.grey5.color,
+  });
+
+  const innerCircle = (isTrue: boolean): ViewStyle => ({
+    height: wp(5.5),
+    width: wp(5.5),
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: isTrue ? shared.primary.color : shared.white.color,
+  });
+
+  const leftLine = (isActiveMark: boolean): ViewStyle => ({
+    position: "absolute",
+    top: hp(2),
+    right: wp(18),
+    width: wp(23),
+    height: 2,
+    backgroundColor: isActiveMark ? shared.primary.color : shared.grey5.color,
+  });
+
+  const textStyle = (isActiveMark: boolean): TextStyle => ({
+    color: isActiveMark ? shared.primary.color : shared.black.color,
+    marginTop: 5,
+  });
+
+  const pageStep = (
+    hasLeftLine: boolean,
+    isActiveMark: boolean,
+    text: string
+  ) => {
+    return (
+      <View
+        style={{
+          alignItems: "center",
+          width: wp(30),
+        }}
+      >
+        {hasLeftLine ? <View style={leftLine(isActiveMark)} /> : null}
+        <View style={outerCircle(isActiveMark)}>
+          <View style={innerCircle(isActiveMark)}>
+            <Icon
+              name="check"
+              type="font-awesome-5"
+              color={shared.white.color}
+              size={13}
+              solid
+            />
+          </View>
+        </View>
+        <Text style={textStyle(isActiveMark)}>{text}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {pageStep(false, true, "Date & Time")}
+      {pageStep(true, false, "Job Detail")}
+      {pageStep(true, false, "Finalize")}
+    </View>
+  );
+}
+
+const styles = makeStyles((theme) => ({}));
--- src/navigation/screens/BookJob.tsx
+++ src/navigation/screens/BookJob.tsx
@@ -6,76 +6,17 @@ import { makeStyles, Text, Icon } from "@rneui/themed";
 import { View, ViewStyle } from "react-native";
 import { sharedStyles } from "../../utils/styles";
 import { useState } from "react";
+import { PageMark } from "../../components/BookJob/PageMark";
 
 export function BookJob() {
   const style = styles();
   const shared = sharedStyles();
   const [index, setIndex] = useState(0);
 
-  const outerCircle = (isTrue: boolean): ViewStyle => ({
-    height: wp(8),
-    width: wp(8),
-    borderRadius: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: isTrue ? "#f7c9aa" : shared.grey5.color,
-  });
-
-  const innerCircle = (isTrue: boolean): ViewStyle => ({
-    height: wp(5.5),
-    width: wp(5.5),
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: isTrue ? shared.primary.color : shared.white.color,
-  });
-
-  const leftLine = (isActiveColor: boolean): ViewStyle => ({
-    position: "absolute",
-    top: hp(2),
-    right: wp(18),
-    width: wp(23),
-    height: 2,
-    backgroundColor: isActiveColor ? shared.primary.color : shared.grey5.color,
-  });
-
-  const checkMark = (
-    hasLeftLine: boolean,
-    isActiveColor: boolean,
-    text: string
-  ) => {
-    return (
-      <View
-        style={{
-          alignItems: "center",
-          width: wp(30),
-        }}
-      >
-        {hasLeftLine ? <View style={leftLine(isActiveColor)} /> : null}
-        <View style={outerCircle(isActiveColor)}>
-          <View style={innerCircle(isActiveColor)}>
-            <Icon
-              name="check"
-              type="font-awesome-5"
-              color={shared.white.color}
-              size={13}
-              solid
-            />
-          </View>
-        </View>
-        <Text>{text}</Text>
-      </View>
-    );
-  };
-
   return (
     <View style={style.container}>
       <View>
-        <View style={{ flexDirection: "row" }}>
-          {checkMark(false, true, "Date & Time")}
-          {checkMark(true, false, "Job Detail")}
-          {checkMark(true, false, "Finalize")}
-        </View>
+        <PageMark />
       </View>
     </View>
   );
