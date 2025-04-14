c4992046f572c1d48ee4774c751a8c573011d252
orchlonn <ch.orchlon@gmail.com>
2025-04-13 21:30:26 -0700

    main input added

---
+++ src/components/MainInput.tsx
@@ -0,0 +1,39 @@
+import { makeStyles, Input, Text } from "@rneui/themed";
+import { sharedStyles } from "../utils/styles";
+import { sharedTextStyles } from "../utils/textStyles";
+
+export function MainInput({ title }: { title: string }) {
+  var shared: any = sharedStyles();
+  const style = styles();
+  const textStyles: any = sharedTextStyles();
+
+  return (
+    <Input
+      placeholder={title}
+      placeholderTextColor="#9E9E9E"
+      containerStyle={style.containerStyle}
+      inputContainerStyle={style.inputContainerStyle}
+      inputStyle={style.inputStyle}
+    />
+  );
+}
+
+const styles = makeStyles((theme) => ({
+  inputContainerStyle: {
+    borderBottomWidth: 0,
+    backgroundColor: "#F5F6F8",
+    borderRadius: 50,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  containerStyle: {
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+    margin: 0,
+  },
+  inputStyle: {
+    textAlignVertical: "center",
+    padding: 0,
+    margin: 0,
+  },
+}));
