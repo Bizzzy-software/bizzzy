aff8fd09d5065f5ddb5d5d37ad65d2f44755a158
orchlonn <ch.orchlon@gmail.com>
2025-04-15 19:36:32 -0700

    Created a seperate job details component for the booking screen and implemented conditional logic to control screen display on the book job interface.

---
+++ src/components/BookJob/JobDetails.tsx
@@ -0,0 +1,37 @@
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import React from "react";
+import { makeStyles, Text } from "@rneui/themed";
+import { View } from "react-native";
+import { sharedStyles } from "../../utils/styles";
+import { sharedTextStyles } from "../../utils/textStyles";
+import { MainInput } from "../MainInput";
+import { PageMark } from "./PageMark";
+
+export function JobDetails() {
+  var textStyles: any = sharedTextStyles();
+  const style = styles();
+  const shared: any = sharedStyles();
+
+  return (
+    <View style={style.jobFormContainer}>
+      <Text style={textStyles.blackMediumSemiBold}>Job Title</Text>
+      <MainInput title="Title of the job" />
+      <Text style={textStyles.blackMediumSemiBold}>Address</Text>
+      <MainInput title="Destination" />
+      <Text style={textStyles.blackMediumSemiBold}>Postal zip</Text>
+      <MainInput title="Postal zip" />
+      <Text style={textStyles.blackMediumSemiBold}>Notes</Text>
+      <MainInput title="You have note for shipper?" />
+    </View>
+  );
+}
+
+const styles = makeStyles((theme) => ({
+  jobFormContainer: {
+    padding: wp(5),
+    gap: hp(1),
+  },
+}));
--- src/navigation/screens/BookJob.tsx
+++ src/navigation/screens/BookJob.tsx
@@ -2,15 +2,16 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import { makeStyles, Text, Icon, Input } from "@rneui/themed";
-import { View, ViewStyle } from "react-native";
+import { makeStyles } from "@rneui/themed";
+import { View } from "react-native";
 import { sharedStyles } from "../../utils/styles";
 import { useState } from "react";
-import { PageMark } from "../../components/BookJob/PageMark";
-import { MainInput } from "../../components/MainInput";
 import { sharedTextStyles } from "../../utils/textStyles";
 import PrimaryButton from "../../components/PrimaryButton";
 import { ScrollView } from "react-native-gesture-handler";
+import { JobDetails } from "../../components/BookJob/JobDetails";
+import { PageMark } from "../../components/BookJob/PageMark";
+import { DateAndTime } from "../../components/BookJob/DateAndTime";
 
 export function BookJob() {
   const style = styles();
@@ -18,24 +19,12 @@ export function BookJob() {
   const textStyles = sharedTextStyles();
   const [index, setIndex] = useState(0);
 
-  const JobForms = () => {
-    return (
-      <View style={style.jobFormContainer}>
-        <Text style={textStyles.blackMediumSemiBold}>Job Title</Text>
-        <MainInput title="Title of the job" />
-        <Text style={textStyles.blackMediumSemiBold}>Address</Text>
-        <MainInput title="Destination" />
-        <Text style={textStyles.blackMediumSemiBold}>Notes</Text>
-        <MainInput title="You have note for shipper?" />
-      </View>
-    );
-  };
-
   return (
     <ScrollView style={style.container}>
       <View style={style.bookingCard}>
         <PageMark />
-        <JobForms />
+        {index == 0 ? <DateAndTime /> : null}
+        {index == 1 ? <JobDetails /> : null}
         <View style={style.buttonContainer}>
           <PrimaryButton buttonTitle="Continue" />
         </View>
@@ -55,10 +44,6 @@ const styles = makeStyles((theme) => ({
     backgroundColor: theme.colors.white,
     borderRadius: 20,
     paddingVertical: hp(3),
-  },
-  jobFormContainer: {
-    padding: wp(5),
-    gap: hp(1),
   },
   buttonContainer: {
     flexDirection: "row",
